c5ec011179997c33e89850a3faf00326076f75f8
YusufBodurr <70092552+YusufBodurr@users.noreply.github.com>
2023-08-08 08:32:59 +0300

    CameraCanAvaliable

---
+++ app/src/main/java/com/example/deneme2/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.deneme2
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.deneme2.ui.theme.Deneme2Theme
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    val REQUEST_IMAGE_CAPTURE = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.image_save)
+        button = findViewById(R.id.btn_take_picture)
+
+        button.setOnClickListener{
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }catch (e: ActivityNotFoundException){
+                //Toast.makeText(context:this,text:"Error: " + e.localizedMessage, Toast.LENGTH_SHORTS).show()
+            }
+
+        }
+        }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
